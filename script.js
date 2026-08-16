347bd76bf0cdfe6cfeefd4a039c3402fadd952b5
futurorazionale <futurorazionale@gmail.com>
2026-08-16 12:53:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.querySelector(".menu-toggle");
-  const nav = document.querySelector(".nav-links");
+  const menuButton =
+    document.querySelector(".menu-toggle") ||
+    document.querySelector("#menu");
+
+  const nav =
+    document.querySelector(".nav-links") ||
+    document.querySelector("#nav");
 
   if (menuButton && nav) {
     menuButton.addEventListener("click", () => {
